237334699bbfd33d63c5670ccf2fbbc8bedf6111
DarkAndrei <a.shwetsov13@gmail.com>
2026-03-25 15:56:23 +0200

    update project config and dependency

--- ForumTestTask.BE/src/Program.cs
+++ ForumTestTask.BE/src/Program.cs
@@ -1,54 +1,66 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.Extensions.FileProviders;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-builder.Services.AddHttpsRedirection(options =>
-{
-    options.HttpsPort = 7165;
-});
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(
+            new JsonStringEnumConverter()
+        );
+    });
 
-// Add CORS policy to allow requests from the React frontend
-builder.Services.AddCors(options =>
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        sqlOptions => sqlOptions.EnableRetryOnFailure()
+    ));
+
+builder.Services.AddCors(o => o.AddPolicy("CorsPolicy", policy =>
+    policy
+      .WithOrigins("https://localhost:3000", "https://localhost", "https://localhost:443", "https://localhost:5000")
+      .AllowAnyMethod()
+      .AllowAnyHeader()
+      .AllowCredentials()
+));
+
+builder.Services.Configure<FormOptions>(options =>
 {
-    options.AddPolicy("AllowAll",
-        policy => policy
-            .WithOrigins("https://localhost:3000")
-            .AllowAnyHeader()
-            .AllowAnyMethod());
+    options.MultipartBodyLengthLimit = 100_000_000;
 });
 
 builder.Services.AddScoped<FileService>();
+builder.Services.AddSingleton<HtmlSanitizerService>();
 
 var app = builder.Build();
 
-// Ensure the database is deleted and created on startup for development purposes
 using (var scope = app.Services.CreateScope())
 {
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-
-    db.Database.EnsureDeleted();
-    db.Database.EnsureCreated();
+    try
+    {
+        var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        db.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Database migration failed: " + ex.Message);
+        throw;
+    }
 }
 
-// using (var scope = app.Services.CreateScope())
-// {
-//     try
-//     {
-//         var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-//         db.Database.Migrate();
-//     }
-//     catch (Exception ex)
-//     {
-//         Console.WriteLine("Database migration failed: " + ex.Message);
-//         throw;
-//     }
-// }
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads")
+    ),
+    RequestPath = "/uploads"
+});
 
-app.UseHttpsRedirection();
-app.UseCors("AllowAll");
+app.UseStaticFiles();
+app.UseCors("CorsPolicy");
 app.UseAuthorization();
 app.MapControllers();
 
